343210302b5c1703f33cd4807c95d3ea566f2a3b
tony <tony.cho@atmel.com>
2014-11-10 18:16:44 +0900

    Update the linux-at91 and wilc-firmware commit SHA including nmi3000 v1.0

--- package/wilc-firmware/wilc-firmware.mk
+++ package/wilc-firmware/wilc-firmware.mk
@@ -5,14 +5,15 @@
 ################################################################################
 
 
-WILC_FIRMWARE_VERSION = 574387edfa61826da6990aa07c37aa70fa78c6d2
+WILC_FIRMWARE_VERSION = 3ce04fe75c49784defc560e04459702e9a98d808
 WILC_FIRMWARE_SITE = https://github.com/linux4sc-wilc3000/wilc_firmware.git
 WILC_FIRMWARE_SITE_METHOD = git
 
 WILC_FIRMWARE_DRIVER_FILE += wilc1000_fw.bin
 WILC_FIRMWARE_DRIVER_FILE += wilc1000_ap_fw.bin
 WILC_FIRMWARE_DRIVER_FILE += wilc1000_p2p_fw.bin
-
+WILC_FIRMWARE_DRIVER_FILE += wifi_firmware.bin
+WILC_FIRMWARE_DRIVER_FILE += bt_fw.bin
 
 #WILC_FIRMWARE_LICENSE = PROPRIETARY
 #WILC_FIRMWARE_REDISTRIBUTE = NO
